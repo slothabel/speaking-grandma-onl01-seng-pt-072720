cd031fff75e92e1d34bcb6406ff0e95d8b2f6355
Yoshi Mejia <yoshimejia00@gmail.com>
2020-07-30 23:18:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma
-  if "I LOVE YOU GRANDMA!" == .upcase
+  if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else HUH?! SPEAK UP, SONNY!
     
