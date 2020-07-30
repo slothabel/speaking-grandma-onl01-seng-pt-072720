73c28edf30fbc7db447637fc9c6211505d803f31
Yoshi Mejia <yoshimejia00@gmail.com>
2020-07-30 23:15:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,14 +5,14 @@ def speak_to_grandma
   else HUH?! SPEAK UP, SONNY!
     
   #elsif condition
-    
+
   end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
