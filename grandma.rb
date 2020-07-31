388592625c6d31f30c1063e826c49a7ec84a2c7c
Yoshi Mejia <yoshimejia00@gmail.com>
2020-07-31 00:18:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}".downcase
-    return "NO, NOT SINCE 1938!"
-  else  "#{phrase}".upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else  "#{phrase}".upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with
